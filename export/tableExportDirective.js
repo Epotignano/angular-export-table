b1e19fa2ea00217e2f064c563c1823dc261bbbd5
epotignano <epotignano@gmail.com>
2015-09-04 19:11:26 -0300

    'Init'

---
+++ export/tableExportDirective.js
@@ -0,0 +1,92 @@
+/**
+ * Created by mmasuyama on 9/2/2015.
+ */
+
+var tableExportCtrl = function($element, $attrs, $document) {
+  var ctrl = this;
+
+    var elementScope = $element.isolateScope();
+
+    ctrl.disableClick = $element[0].innerHTML.match("te-option");
+
+    var defaultOptions = {
+        type: 'pdf',
+        escape: false
+    };
+
+    function exportTable() {
+        ctrl.tableElement.tableExport({
+            type: elementScope.ctrl.exportOptions.type || defaultOptions.type,
+            escape: elementScope.ctrl.exportOptions.escape || defaultOptions.escape
+        })
+    }
+
+    function setProperty (obj, param, value) {
+        Object.defineProperty(ctrl[obj], param, {
+            value: value,
+            writable: true,
+            configurable: true,
+            enumerable: true
+        })
+    }
+
+
+    if(elementScope.ctrl.tableSelector) {
+        ctrl.tableElement = $document.find(elementScope.ctrl.tableSelector);
+    }
+
+    //TODO allow to define diiferents methods
+    $element.on('click', function(e){
+        if(!ctrl.disableClick){
+            exportTable();
+        }
+    });
+
+    //TODO allow to define differents tags (button, select, input, etc)
+
+
+    //exports
+
+    angular.extend(ctrl,{
+        defaultOptions : defaultOptions,
+        exportTableFn : exportTable,
+        setProperty : setProperty
+    })
+};
+
+var compileExportTable = function($element, $attrs, transcludeFn) {
+
+};
+
+angular.module('ceibo.components.table.export', [])
+  .controller('tableExportCtrl', tableExportCtrl)
+  .directive('tableExport', function(){
+      return {
+          //require: 'table',
+          scope: {
+              /**
+               * options : {
+               *     formats : {
+               *        pdf: //boolean,
+               *        xls: //boolean
+               *     }
+               * }
+               *
+               *
+               **/
+              exportOptions: '=',
+              /*
+                selector : { selectorType: selector }
+               */
+
+              tableSelector: '=' //selector that this element is refering refers.
+
+          }, //isolate or not
+          restrict : 'AEC', //A = attribute, C = class, E = Element
+          controller: 'tableExportCtrl as ctrl',
+          compile: compileExportTable,
+          bindToController: true //true or false
+      }
+  })
+
+;
---
+++ export/tableExportOptionDirective.js
@@ -0,0 +1,38 @@
+/**
+ * Created by mmasuyama on 9/3/2015.
+ */
+var teOptionCtrl = function($scope, $element, $attrs) {
+  var ctrl = this;
+    var elementScope = $element.isolateScope().ctrl;
+
+    $element.on('click', function(e){
+        console.log($scope);
+        elementScope.tableExportCtrl.setProperty('exportOptions', elementScope.teParam,elementScope.teValue);
+        elementScope.tableExportCtrl.exportTableFn()
+    })
+};
+
+var linkFn = function($scope, $element, $attrs, dirContrl) {
+    var elementScope = $element.isolateScope();
+    elementScope.ctrl.tableExportCtrl = dirContrl;
+
+};
+
+angular.module('ceibo.components.table.export')
+  .controller('teOptionCtrl', teOptionCtrl)
+  .directive('teOption', function(){
+      return {
+          templateUrl: '',
+          require: '^tableExport',
+          scope: {
+              teParam: '=',
+              teValue: '='
+          }, //isolate or not
+          restrict : 'AC', //A = attribute, C = class, E = Element
+          controller: 'teOptionCtrl as ctrl',
+          link: linkFn,
+          bindToController: true //true or false
+      }
+  })
+
+;
